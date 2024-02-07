b4bf4c9ece2c97aa9de82dcc233701547463e22b
Fausto <300501@moron.gob.ar>
2024-02-07 14:21:56 -0300

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { NgIconsModule } from '@ng-icons/core';
+import { heroUsers } from '@ng-icons/heroicons/outline';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'cv-1';
+export class AppComponent implements OnInit {
+  
+icon!:any;
+
+ngOnInit(): void {
+  
+}
+
+  constructor(private http:HttpClient){}
+
+  
+ 
 }
